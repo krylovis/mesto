ec6bd417ed4c3498d291f235afbe36abd66a61f9
Ivan Krylov <kis.226@yandex.ru>
2023-01-15 14:06:19 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,13 @@ let profileEditButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let popupContainer = document.querySelector('.popup__container');
-let nameInput = document.querySelector('#inputName');
-let jobInput = document.querySelector('#inputJob');
+let inputName = document.querySelector('#inputName');
+let inputJob = document.querySelector('#inputJob');
 
 
 profileEditButton.addEventListener('click', function () {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileSubtitle.textContent;
+  inputName.value = profileName.textContent;
+  inputJob.value = profileSubtitle.textContent;
   popup.classList.toggle('popup_opened');
 });
 
@@ -24,8 +24,8 @@ function closePopup() {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputJob.value;
   closePopup();
 }
 
