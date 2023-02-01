e2f1af7399f70c465f0832a536ce26ec5d657108
Krylov Ivan <krylovis@mchs.ru>
2023-02-01 10:13:25 +0300

    add new card

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,44 +1,18 @@
+import { initialCards } from './initialCards.js';
+
 const elements = document.querySelector('.elements');
-const profileAddButton = document.querySelector('.profile__add-button');
 const elementTemplate = document.querySelector('#element-template').content;
 
-const inputPlaceName = document.querySelector('#input-place-name');
-const inputPlaceLink = document.querySelector('#input-place-link');
+export function renderCards() {
+  elements.innerHTML = '';
+  initialCards.forEach(item => {
+    const element = elementTemplate.cloneNode(true);
+  
+    element.querySelector('.element__title').textContent = item.name;
+    element.querySelector('.element__image').src = item.link;
+  
+    elements.append(element);
+  });
+}
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-initialCards.forEach(item => {
-  const element = elementTemplate.cloneNode(true);
-  console.log('item.name', item.name);
-  console.log('element', element);
-
-  element.querySelector('.element__title').textContent = item.name;
-  element.querySelector('.element__image').src = item.link;
-
-  elements.append(element);
-});
+renderCards();
--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,42 @@
+import { initialCards } from './initialCards.js';
+import { renderCards } from './elements.js';
+
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
+let popupTitle = document.querySelector('.popup__title');
+
 let profileEditButton = document.querySelector('.profile__edit-button');
-let profileName = document.querySelector('.profile__name');
+let profileAddButton = document.querySelector('.profile__add-button');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let popupContainer = document.querySelector('.popup__container');
+let profileName = document.querySelector('.profile__name');
+
+let profileForm = document.querySelector('form[name="profileForm"]');
 let inputName = document.querySelector('#inputName');
 let inputJob = document.querySelector('#inputJob');
 
+let newPlaceForm = document.querySelector('form[name="newPlaceForm"]');
+const inputPlaceName = document.querySelector('#inputPlaceName');
+const inputPlaceLink = document.querySelector('#inputPlaceLink');
 
-profileEditButton.addEventListener('click', function () {
-  inputName.value = profileName.textContent;
-  inputJob.value = profileSubtitle.textContent;
-  popup.classList.toggle('popup_opened');
-});
-
-popupCloseButton.addEventListener('click', function () {
+popupCloseButton.addEventListener('click', () => {
   closePopup();
 });
 
 function closePopup() {
   popup.classList.remove('popup_opened');
+  profileForm.classList.remove('popup__form_opened');
+  newPlaceForm.classList.remove('popup__form_opened');
 };
+
+// Редактировать профиль
+
+profileEditButton.addEventListener('click', () => {
+  popupTitle.textContent = 'Редактировать профиль';
+  popup.classList.toggle('popup_opened');
+  profileForm.classList.toggle('popup__form_opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileSubtitle.textContent;
+});
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -29,4 +45,27 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
-popupContainer.addEventListener('submit', handleFormSubmit);
+profileForm.addEventListener('submit', handleFormSubmit);
+
+// Добавить новое место
+
+profileAddButton.addEventListener('click', () => {
+  popupTitle.textContent = 'Новое место';
+  popup.classList.toggle('popup_opened');
+  newPlaceForm.classList.toggle('popup__form_opened');
+});
+
+function addCard(evt) {
+  evt.preventDefault();
+  let card = {
+    name: '',
+    link: '',
+  };
+  card.name = inputPlaceName.value;
+  card.link = inputPlaceLink.value;
+  initialCards.unshift(card);
+  renderCards();
+  closePopup();
+}
+
+newPlaceForm.addEventListener('submit', addCard);
