466bbac495bf7696566ef75550264bc22d70e0f7
Ivan Krylov <kis.226@yandex.ru>
2023-02-04 13:02:13 +0300

    delete cardsRenderer js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 import { initialCards } from './initialCards.js';
-import { renderCards } from './cardsRenderer.js';
 
 let popupProfileForm = document.querySelector('.popup_type_profile-form');
 let popupNewPlace = document.querySelector('.popup_type_new-place');
@@ -48,6 +47,19 @@ function handleFormSubmit(event) {
 
 profileForm.addEventListener('submit', handleFormSubmit);
 
+// Открыть фото
+
+const popupPhoto = document.querySelector('.popup__photo');
+const popupFigcaption = document.querySelector('.popup__figcaption');
+
+function openPlacePhoto(event) {
+  event.preventDefault();
+  popupPhoto.src = event.target.src;
+  const placeTitle = event.target.parentElement.querySelector('.element__title');
+  popupFigcaption.textContent = placeTitle.textContent;
+  popupPlacePhoto.classList.toggle('popup_opened');
+}
+
 // Добавить новое место
 
 profileAddButton.addEventListener('click', () => {
@@ -70,15 +82,39 @@ function addCard(event) {
 
 newPlaceForm.addEventListener('submit', addCard);
 
-// Открыть фото
+// Создать новую карточку
 
-const popupPhoto = document.querySelector('.popup__photo');
-const popupFigcaption = document.querySelector('.popup__figcaption');
+const cardsContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#element-template').content;
 
-export function openPlacePhoto(event) {
-  event.preventDefault();
-  popupPhoto.src = event.target.src;
-  const placeTitle = event.target.parentElement.querySelector('.element__title');
-  popupFigcaption.textContent = placeTitle.textContent;
-  popupPlacePhoto.classList.toggle('popup_opened');
+function renderCards() {
+  cardsContainer.innerHTML = '';
+  initialCards.forEach(item => {
+    const element = cardTemplate.cloneNode(true);
+    const buttonDelete = element.querySelector('.element__trash');
+    const cardImage = element.querySelector('.element__image');
+    const card = element.querySelector('.element');
+  
+    element.querySelector('.element__title').textContent = item.name;
+    cardImage.alt = item.name;
+    cardImage.src = item.link;
+
+    cardImage.addEventListener('click', (event) => {
+      event.preventDefault();
+      openPlacePhoto(event);
+    });
+
+    element.querySelector('.element__like').addEventListener('click', (event) => {
+      event.preventDefault();
+      event.target.classList.toggle('element__like_active');
+    });
+    
+    buttonDelete.addEventListener('click', () => {
+      card.remove();
+    });
+  
+    cardsContainer.append(element);
+  });
 }
+
+renderCards();
