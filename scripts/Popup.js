00b437a8f9f53e5a74d60140a5720dce444ac54e
Ivan Krylov <kis.226@yandex.ru>
2023-03-25 15:24:54 +0300

    fix popup

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -6,12 +6,12 @@ export default class Popup {
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', _closeByEscape);
+    document.addEventListener('keydown', this._closeByEscape);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', _closeByEscape);
+    document.removeEventListener('keydown', this._closeByEscape);
   }
 
   _handleEscClose(event) {
